451dda43d7f6abcadc04746ca7dbe7ebfac84f2b
lovemilk <zhuhansan666@outlook.com>
2024-01-06 23:40:10 +0800

    F C!

--- src/routers/html.py
+++ src/routers/html.py
@@ -1,29 +1,52 @@
 from pathlib import Path
-from time import time as get_time
+# import urllib.parse
+# from time import time as get_time
+# from random import choice
 from fastapi import APIRouter
-from fastapi.responses import HTMLResponse, Response
-from fastapi.exceptions import HTTPException
+from fastapi.responses import HTMLResponse, FileResponse, Response
+# from fastapi.exceptions import HTTPException
 from loguru import logger
+
 error_logger = logger.opt(exception=True)
 
 router = APIRouter(tags=['page'])
 
+# SPACES = ['', ' ', '　']
+# FUCK_STRING = 'fuckTo=CyanChanges'
+
 
 @router.get("/favicon.ico")
 async def favicon():
-    from src.shared import session
-    url = (f'https://content.cyans.me/static/lovemilk-hosted/oms/favicon.ico?'
-           f'{get_time()}={get_time()}&fuckTo=CyanChanges')
-    logger.debug('request favicon URL: {}', url)
-    try:
-        response = await session.get(url, headers={
-            'Origin': 'https://content.cyans.me',
-            'Referer': 'https://content.cyans.me',
-        })
-        return Response(content=await response.read(), media_type='image')
-    except Exception:
-        error_logger.error('failed to get favicon')
-        raise HTTPException(status_code=404)
+    # from src.shared import session
+    # fuck_to = ''
+    # for char in FUCK_STRING:
+    #     fuck_to += char + choice(SPACES)
+    # arg, val = fuck_to.split('=', 2)
+    #
+    # args = {
+    #     get_time(): get_time(),
+    #     arg: val
+    # }
+    #
+    # url = f'https://content.cyans.me/static/lovemilk-hosted/oms/favicon.ico?' + urllib.parse.urlencode(args)
+    # logger.debug('request favicon URL: {}', url)
+    # try:
+    #     response = await session.get(url, headers={
+    #         'Origin': 'https://content.cyans.me',
+    #         'Referer': 'https://content.cyans.me',
+    #         'User-Agent': 'Chromium:Fuck Cyan!'
+    #     })
+    #     if not response.headers.get('Content-Type', '').startswith('image'):
+    #         raise HTTPException(status_code=400, detail='一定是 CyanChanges 干的!')
+    #
+    #     return Response(content=await response.read(), media_type='image')
+    # except HTTPException:
+    #     raise
+    # except Exception as e:
+    #     error_logger.error('failed to get favicon')
+    #     raise HTTPException(status_code=404, detail=repr(e))
+    return FileResponse(Path('static/favicon.ico'))
+
 
 @router.get('/', response_class=HTMLResponse)
 async def index():
