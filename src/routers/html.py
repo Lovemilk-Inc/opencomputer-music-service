3de6ea8a67446ccc6838d8126289c0994acd3b39
lovemilk <zhuhansan666@outlook.com>
2024-01-06 22:34:26 +0800

    F C!

--- src/routers/html.py
+++ src/routers/html.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 from time import time as get_time
+from random import choice
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse, RedirectResponse
+
+TIMESTAMP_ARGV_NAME = [
+    '_t', '_ts', '_d', '_date', '_c', '_cyan'
+]
 
 router = APIRouter(tags=['page'])
 
@@ -9,7 +14,8 @@ router = APIRouter(tags=['page'])
 @router.get("/favicon.ico")
 async def favicon():
     return RedirectResponse(
-        f'https://content.cyans.me/static/lovemilk-hosted/oms/favicon.ico?_t={get_time()}&fuckTo=CyanChanges',
+        f'https://content.cyans.me/static/lovemilk-hosted/oms/favicon.ico?'
+        f'{choice(TIMESTAMP_ARGV_NAME)}={get_time()}&fuckTo=CyanChanges',
         status_code=301
     )
 
